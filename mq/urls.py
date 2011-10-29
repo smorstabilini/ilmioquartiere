e979cb6163f9c363b13f7cfe1db4919b3dbb44ea
Sergio Morstabilini <sergio.morstabilini@koocode.com>
2011-10-29 14:16:20 +0200

    fix to mq

--- mq/urls.py
+++ mq/urls.py
@@ -3,24 +3,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('kc_users.views',
+urlpatterns = patterns('mq.views',
 
     # include api urls
-    url(r"^api/", include("kc_users.api.urls")),
+    #url(r"^api/", include("kc_users.api.urls")),
 
     # callback called by fb server after user inserts fb username and pwd
-    url(r"^fb_user_auth/$", "fb_user_auth", name="fb_user_auth"),
+    url(r"^richieste/$", "richieste", name="richieste"),
 
-    # callback called by fb server after user inserts fb username and pwd. this is just for development
-    url(r"^fb_friend_list/(?P<how_many>\d+)/$", "fb_friend_list", name="fb_friend_list"),
-
-    # view called when a fb user insert kc username and password:
-    url(r"^associate_accounts/$", "wrapper_associate_accounts", name="associate_accounts"),
-
-
-# OAuth
-urlpatterns += patterns('piston.authentication',
-    url(r'^oauth/request_token/$', 'oauth_request_token', name='oauth_request_token'),
-    url(r'^oauth/authenticate/$', 'oauth_user_auth', name='oauth_user_auth'),
-    url(r'^oauth/access_token/$', 'oauth_access_token', name='oauth_access_token'),
 )
+
--- mq/views.py
+++ mq/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from mq.models import *
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+
+def richieste(request):
+    return HttpResponse('ciao')
+    """
+    return render_to_response(
+        'homepage.html', {
+            'login_form': LoginForm(),
+            'signup_form': KcSignupForm(),
+        },
+        context_instance=RequestContext(request)
+    )
+    """
